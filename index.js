b63aede7f9c6e30b2e528682da07ff137bfecc5a
Heley Chen <zccz14@outlook.com>
2016-05-22 20:05:43 +0800

    update vhd generate algorithm

--- index.js
+++ index.js
@@ -3,9 +3,68 @@ const buffer = require('buffer');
 
 class VHD {
     constructor(size = 0) {
-        var appendix = fs.readFileSync('./lib/vhd_appendix');
         this.buffer = new buffer.Buffer.alloc(size + 512);
-        appendix.copy(this.buffer, size, 0, 512);
+        this.footer = this.buffer.slice(size, size + 512);
+        this.originSize = this.footer.slice(0x028, 0x030);
+        this.currentSize = this.footer.slice(0x030, 0x038);
+        this.CHS = this.footer.slice(0x038, 0x03C);
+        this.UUID = this.footer.slice(0x044, 0x054);
+        this.checkSum = this.footer.slice(0x040, 0x044);
+
+        var footer = fs.readFileSync('./lib/vhd_footer_default');
+
+        // Timestamp
+        footer.writeInt32BE(~~((Date.now() - 946728000000) / 1000), 0x018);
+
+        // origin size
+        footer.writeIntBE(size, 0x028, 8);
+        // current size
+        footer.writeIntBE(size, 0x030, 8);
+
+        // CHS Algorithm XXXX 04 11 in 0x038
+        footer.writeInt32BE(this.getCHS(size), 0x038);
+
+        // UUID
+        for (var i = 0x044; i < 0x054; i++)
+            footer[i] = ~~(Math.random() * 256);
+
+        // CheckSum
+        footer.writeInt32BE(
+            ~(
+                footer.slice(0x000, 0x040).reduce((a, b) => a + b, 0) +
+                footer.slice(0x044, 0x200).reduce((a, b) => a + b, 0)
+            ),
+            0x040
+        );
+
+        footer.copy(this.footer, 0, 0, 512);
+
+        return this;
+    }
+
+    getCHS(size) {
+        var totalSectors = Math.min(~~(size / 512), 65535 * 16 * 255);
+        if (totalSectors >= 65535 * 16 * 63) {
+            var sectorsPerTrack = 255;
+            var heads = 16;
+            cylinderTimeHeads = ~~(totalSectors / sectorsPerTrack);
+        } else {
+            var sectorsPerTrack = 17;
+            var cylinderTimeHeads = ~~(totalSectors / sectorsPerTrack);
+            var heads = Math.max(~~((cylinderTimeHeads + 1023) / 1024), 4);
+            if (cylinderTimeHeads >= (heads * 1024) || heads > 16) {
+                sectorsPerTrack = 31;
+                heads = 16;
+                cylinderTimeHeads = ~~(totalSectors / sectorsPerTrack);
+            }
+            if (cylinderTimeHeads >= (heads * 1024)) {
+                sectorsPerTrack = 63;
+                heads = 16;
+                cylinderTimeHeads = ~~(totalSectors / sectorsPerTrack);
+            }
+        }
+        var cylinders = ~~(cylinderTimeHeads / heads);
+        return (cylinders << 16) + (heads << 8) + sectorsPerTrack;
     }
 
     save(url) {
@@ -17,7 +76,7 @@ class VHD {
         this.buffer = fs.readFileSync(url);
         return this;
     }
-    
+
     static load(url) {
         return (new VHD()).load(url);
     }
